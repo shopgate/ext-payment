3261f2614429e4c709cc2c6815c9f4546fde4474
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2018-06-05 12:31:54 +0200

    Removed a debug log output and corrected a comment to tell what the code is actually supposed to do

--- extension/payment/mockedRules/LimitCashOnDeliveryToHermes.js
+++ extension/payment/mockedRules/LimitCashOnDeliveryToHermes.js
@@ -4,12 +4,11 @@
  * @returns {Promise<PaymentMethod[]>|PaymentMethod[]}
  */
 module.exports = async (context, input) => {
-  console.log(input.checkout)
   if (input.checkout.shippingMethod && input.checkout.shippingMethod.id === 'hermes') {
     return {paymentMethods: input.paymentMethods}
   }
 
-  // payment rule for DE
+  // payment rule for "shipping is not hermes"
   return {paymentMethods: input.paymentMethods.filter(paymentMethod => (
     paymentMethod.id === 'authorizenet' || paymentMethod.id === 'paypal'
   ))}
